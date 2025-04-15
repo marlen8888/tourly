ed0a278cd812564ea586639f32fdd93bed258bac
marlen8888 <marlen.raimbekov@alatoo.edu.kg>
2025-04-15 23:39:58 +0600

    fixed mistakes and add bottoms

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,11 +21,14 @@ navToggleEvent(navElemArr);
 navToggleEvent(navLinks);
 
 const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-gp-top]");
 
 window.addEventListener("scroll", function () {
     if (window.scrollY >= 200) {
         header.classList.add("active");
+        goTopBtn.classList.add("active");
     } else {
         header.classList.remove("active");
+        goTopBtn.classList.remove("active");
     }
 });
